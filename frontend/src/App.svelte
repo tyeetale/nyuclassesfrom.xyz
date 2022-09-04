6ccea917ceb44e86d1bde2b4feee3b88d650e282
Tyeetale <17817587+tyeetale@users.noreply.github.com>
2022-09-04 23:49:30 +0700

    add tailwind

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,45 +1,19 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
-</script>
+  </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+ 
+  <h1>nyuclassesfrom.xyz</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+  <p>try words, phrases, titles, subjects, schools, course numbers, instructor names, instructor mode, grading, components, and more. 
+    You can also look for exact phrases and prefix matches.</p>
+ 
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
+   <p>
+    made with ❤ by <a href="https://github.com/tyeetale" target="_blank">tyeetale</a> & <a href="https://github.com/sveltejs/kit#readme" target="_blank">sheldon</a> using 
   </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+  
 </style>
