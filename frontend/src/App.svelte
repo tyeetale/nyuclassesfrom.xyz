8dd62ef7163557738fc574781cb7183e95944fdc
Tyeetale <17817587+tyeetale@users.noreply.github.com>
2022-09-05 00:25:31 +0700

    reset files

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
+import SearchBar from "./lib/SearchBar.svelte";
+
+
   </script>
 
 <main>
- 
-  <h1>nyuclassesfrom.xyz</h1>
+  <!-- Title -->
+  <h1 class="text-3xl font-bold underline">nyuclassesfrom.xyz</h1>
 
-  <p class="w-2">try words, phrases, titles, subjects, schools, course numbers, instructor names, instructor mode, grading, components, and more. 
+  <!-- Searchbar -->
+  <SearchBar />
+  <!-- Search Directions -->
+  <p class="p-3">try words, phrases, titles, subjects, schools, course numbers, instructor names, instructor mode, grading, components, and more. <br>
     You can also look for exact phrases and prefix matches.</p>
  
-
+  <!-- Footer -->
    <p>
     made with ❤ by <a href="https://github.com/tyeetale" target="_blank">tyeetale</a> & <a href="https://github.com/sveltejs/kit#readme" target="_blank">sheldon</a> using 
   </p>
