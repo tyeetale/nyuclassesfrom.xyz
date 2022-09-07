e628da632b434200d4305621ba0dc1d5b352a15b
nh8157 <nh8157@hotmail.com>
2022-09-07 10:06:09 +0800

    Improved JSON fetching & parsing efficiency

--- data/json_reader/src/fetch/fetch.rs
+++ data/json_reader/src/fetch/fetch.rs
@@ -1,36 +1,51 @@
 use std::collections::HashMap;
-
-use futures::future::join_all;
+use tokio;
 
 use crate::fetch::json_structure::{CourseInfo, SubjectName};
 use crate::fetch::url_builder::UrlBuilder;
 use crate::fetch::types::Error;
 
-pub async fn fetch_from_api(year: u32, semester: String) -> Result<(), Error> {
+pub async fn fetch_subjects() -> Result<HashMap<String, HashMap<String, SubjectName>>, Error> {
     // first step, fetch the course and school info 
     let url = UrlBuilder::build_subjects_endpoint_url()?;
 
-    let res = match reqwest::get(url).await {
+    match reqwest::get(url).await {
         Ok(res) => {
             match res.json::<HashMap<String, HashMap<String, SubjectName>>>().await {
-                Ok(map) => map,
+                Ok(map) => return Ok(map),
                 Err(_) => return Err(Error::DecodeContentFailed),
             }
         },
         Err(_) => return Err(Error::FetchContentFailed),
     };
+}
 
+pub async fn fetch_courses(year: u32, semester: &String, school_subject_catalog: &HashMap<String, HashMap<String, SubjectName>>) -> Result<(), Error> {
     // second step, iterate through all schools and their corresponding subjects
     // send get requests to the api
-    let mut futures = Vec::new();
-    for (school, subject_list) in &res {
+    let mut url_handles = Vec::new();
+    for (school, subject_list) in school_subject_catalog {
         for (subject, _) in subject_list {
             let url = UrlBuilder::build_courses_endpoint_url(year, &semester, school, subject)?;
-            futures.push(reqwest::get(url));
+            url_handles.push(tokio::spawn(reqwest::get(url)));
         }
     }
 
-    println!("{:?}", join_all(futures.into_iter()).await);
+    // wait for downloading of jsons
+    let url_len = url_handles.len();
+    let mut parse_handles = Vec::new();
+    for (i, jh) in url_handles.into_iter().enumerate() {
+        let res = jh.await.unwrap().unwrap();
+        println!("Response retrieved: {}/{}", i + 1, url_len);
+        parse_handles.push(tokio::spawn(res.json::<Vec<CourseInfo>>()));
+    }
+
+    // wait for completion of parsing
+    let parse_len = parse_handles.len();
+    for (i, jh) in parse_handles.into_iter().enumerate() {
+        let _res = jh.await.unwrap().unwrap();
+        println!("JSON parsed: {}/{}", i + 1, parse_len);
+    }
 
     Ok(())
 }
--- data/json_reader/src/main.rs
+++ data/json_reader/src/main.rs
@@ -1,8 +1,10 @@
 mod fetch;
 
-use crate::fetch::fetch::fetch_from_api;
+use crate::fetch::fetch::{fetch_subjects, fetch_courses};
 
 #[tokio::main]
 async fn main() {
-    fetch_from_api(2022, String::from("fa")).await.unwrap();
+    if let Ok(school_subject_catalog) = fetch_subjects().await {
+        fetch_courses(2022, &String::from("fa"), &school_subject_catalog).await.unwrap();
+    }
 }
