54778550ade7f5f0b56f1b8549330fc83febb310
Tyeetale <17817587+tyeetale@users.noreply.github.com>
2022-10-18 13:45:56 +0700

    Update App.svelte

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -66,7 +66,7 @@
         also look for exact phrases and prefix matches.
       </p>
 
-      <div class="flex flex-nowrap justify-center mt-4">
+      <div class="flex flex-nowrap justify-center mt-4 space-x-4">
         <!-- ADD: <ThemeToggle /> -->
         <a
           href="https://github.com/tyeetale/nyuclassesfrom.xyz"
@@ -84,6 +84,25 @@
             />
           </svg>
         </a>
+
+        <div
+          class="text-slate-400 hover:text-slate-500 dark:hover:text-slate-300"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-10 h-10"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+            />
+          </svg>
+        </div>
       </div>
     </div>
   </div>
