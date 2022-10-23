dc41387e095e17f3e66b72516bc1b9a68e52f6d0
Tyeetale <17817587+tyeetale@users.noreply.github.com>
2022-10-23 14:59:03 +0700

    add light/dark mode

---
+++ frontend/src/lib/ThemeSwitch/ThemeSwitch.svelte
@@ -0,0 +1,70 @@
+<script>
+  let darkMode = true;
+
+  function handleSwitchDarkMode() {
+    darkMode = !darkMode;
+
+    localStorage.setItem("theme", darkMode ? "dark" : "light");
+
+    darkMode
+      ? document.documentElement.classList.add("dark")
+      : document.documentElement.classList.remove("dark");
+  }
+
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+    darkMode = true;
+  } else {
+    document.documentElement.classList.remove("dark");
+    darkMode = false;
+  }
+</script>
+
+<div>
+  <!-- <div class="text-slate-400 hover:text-slate-500 dark:hover:text-slate-300">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-10 h-10"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+      />
+    </svg>
+  </div> -->
+  <input
+    checked={darkMode}
+    on:click={handleSwitchDarkMode}
+    type="checkbox"
+    id="theme-toggle"
+  />
+  <label for="theme-toggle" />
+</div>
+
+<style lang="postcss">
+  #theme-toggle {
+    @apply invisible;
+  }
+
+  #theme-toggle + label {
+    @apply inline-block cursor-pointer h-10 w-10 rounded-full duration-300 content-[''];
+  }
+
+  #theme-toggle:not(:checked) + label {
+    @apply bg-slate-400;
+  }
+
+  #theme-toggle:checked + label {
+    @apply bg-transparent;
+    box-shadow: inset -16px -10px 1px 1px #94a3b8;
+  }
+</style>
