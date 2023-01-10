1abaaae45ebe45da9367a5546f9d364c2f492b74
Tyeetale <17817587+tyeetale@users.noreply.github.com>
2023-01-10 13:01:41 +0700

    Revert "fine tune google analytics"
    This reverts commit 75d4a00c021434618d497aed59f112c0d002d555.

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,25 +1,26 @@
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import * as gtag from "../ga/gtag";
+import Script from "next/script";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
+
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
       </ThemeProvider>
