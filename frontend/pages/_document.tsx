7abcac79c121dcbe73ddee5d9261764781798a62
Tyeetale <17817587+tyeetale@users.noreply.github.com>
2023-01-18 20:33:19 +0800

    test ga with new structure
    https://frontend-digest.com/using-nextjs-with-google-analytics-and-typescript-620ba2359dea

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,12 +1,29 @@
-import NextDocument, { Head, Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "../components/ga/gtag";
 
-type Props = {};
-
-class Document extends NextDocument<Props> {
+export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+          `,
+            }}
+          />
+        </Head>
         <body className="dark:bg-darkPurple">
           <Main />
           <NextScript />
@@ -15,5 +32,3 @@ class Document extends NextDocument<Props> {
     );
   }
 }
-
-export default Document;
